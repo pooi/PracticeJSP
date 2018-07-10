d21816ddc6f1542cd10ecd0fc0b0103801cf7e09
pooi <ldayou@naver.com>
2018-07-10 20:07:17 +0900

    Add features   * DB를 이용한 로그인 학습

---
+++ PracticeJSP/src/com/javalec/ex/LoginOk.java
@@ -0,0 +1,86 @@
+package com.javalec.ex;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoingOk
+ */
+@WebServlet("/LoginOk")
+public class LoginOk extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private Connection connection;
+	private Statement stmt;
+	
+	private String name, id, pw, phone1, phone2, phone3, gender;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginOk() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		doGet(request, response);
+		id = request.getParameter("id");
+		pw = request.getParameter("pw");
+		
+		String query = String.format("SELECT * FROM member WHERE id='%s' and pw='%s';", id, pw);
+		System.out.println(query);
+		
+		try {
+			connection = DBConn.getMySqlConnection();
+			stmt = connection.createStatement();
+			ResultSet resultSet = stmt.executeQuery(query);
+			
+			while(resultSet.next()) {
+				name = resultSet.getString("name");
+				id = resultSet.getString("id");
+				pw = resultSet.getString("pw");
+				phone1 = resultSet.getString("phone1");
+				phone2 = resultSet.getString("phone2");
+				phone3 = resultSet.getString("phone3");
+				gender = resultSet.getString("gender");
+			}
+			
+			HttpSession httpSession = request.getSession();
+			httpSession.setAttribute("name", name);
+			httpSession.setAttribute("id", id);
+			httpSession.setAttribute("pw", pw);
+			
+			response.sendRedirect("loginResult.jsp");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(stmt != null) stmt.close();
+				if(connection != null) connection.close();
+			}catch(Exception e) {}
+		}
+	}
+
+}
